9fc81a70081c1a4119dfd9840b75493b432a4fe1
Alex Evans <alex.evans.d@gmail.com>
2023-09-25 21:14:06 -0500

    Add Home, Layout, Career, and Side Projects pages

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 
 from flask import Flask, render_template
+import json
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/index")
+@app.route("/home")
 def index():
-	return render_template("index.html")
+	return render_template("home.html")
+
+@app.route('/career')
+def career():
+	with open('data/career.json', 'r') as json_file:
+		career_data = json.load(json_file)
+	sorted_career = sorted(career_data, key=lambda x: x["rank"])
+	return render_template('career.html', careers=sorted_career)
+
+@app.route('/side-projects')
+def side_projects():
+    return render_template('side_projects.html')
 
 
 if __name__ == '__main__':
