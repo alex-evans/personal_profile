9379528824f978bb470c919bd6da2885e9b85852
Alex Evans <alex.evans.d@gmail.com>
2023-10-07 06:47:49 -0500

    Add side-projects page

--- app.py
+++ app.py
@@ -11,16 +11,21 @@ app = Flask(__name__)
 def index():
 	return render_template("home.html")
 
+
 @app.route('/career')
 def career():
 	with open('data/career.json', 'r') as json_file:
 		career_data = json.load(json_file)
-	sorted_career = sorted(career_data, key=lambda x: x["rank"])
+	sorted_career = sorted(career_data, key=lambda x: x["rank"], reverse=True)
 	return render_template('career.html', careers=sorted_career)
+
 
 @app.route('/side-projects')
 def side_projects():
-    return render_template('side_projects.html')
+	with open('data/side_projects.json', 'r') as json_file:
+		side_projects_data = json.load(json_file)
+	sorted_side_projects = sorted(side_projects_data, key=lambda x: x["rank"], reverse=True)
+	return render_template('side_projects.html', projects=sorted_side_projects)
 
 
 if __name__ == '__main__':
